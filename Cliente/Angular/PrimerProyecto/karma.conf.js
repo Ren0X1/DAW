455a799d8418df825e18323207e7322d4e2dfea7
Alejandro Mendoza <34504445+Ren0X1@users.noreply.github.com>
2022-02-22 11:16:47 +0100

    Primer Proyecto Angular

--- Cliente/Angular/PrimerProyecto/karma.conf.js
+++ Cliente/Angular/PrimerProyecto/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/primera-aplicacion'),
+      dir: require('path').join(__dirname, './coverage/primer-proyecto'),
       subdir: '.',
       reporters: [
         { type: 'html' },

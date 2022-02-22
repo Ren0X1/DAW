4580ab32e760461fa0e2f6b5e9da7eef9b6c6c4b
Alejandro Mendoza <34504445+Ren0X1@users.noreply.github.com>
2022-02-22 13:27:03 +0100

    Primer proyecto de angular actualizado

--- Cliente/Angular/PrimerProyecto/src/app/app.component.ts
+++ Cliente/Angular/PrimerProyecto/src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'PrimerProyecto';
+    titulo = 'Primer Proyecto Angular';
+    msg = 'Adios';
+    msg2 = 'Hola';
+    mostrar = false;
+    mensaje = () => {
+        this.mostrar = !this.mostrar
+        if (this.mostrar) {
+            this.msg2 = this.msg
+            this.msg = 'Hola'
+        } else {
+            this.msg2 = this.msg
+            this.msg = 'Adios'
+        }
+    }
 }

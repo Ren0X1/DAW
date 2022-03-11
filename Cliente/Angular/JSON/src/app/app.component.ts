decbe4728c77a76400d37a7bb65b0a81917312cb
Alejandro Mendoza <34504445+Ren0X1@users.noreply.github.com>
2022-03-11 09:29:16 +0100

    JSON

--- Cliente/Angular/JSON/src/app/app.component.ts
+++ Cliente/Angular/JSON/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { KeyValue } from '@angular/common';
 import { Component } from '@angular/core';
 import { UsuariosService } from './usuarios.service';
 
@@ -9,11 +10,35 @@ import { UsuariosService } from './usuarios.service';
 export class AppComponent {
   title = 'JSON';
   listaUsuarios:any[]=[];
+  ordenOriginal = (a: KeyValue<number,string>, b: KeyValue<number,string>): number => {
+    return 0;
+  }
   constructor(private usuarios:UsuariosService) {}
   ngOnInit() {
     this.usuarios.getUsuario$().subscribe({
       next: (personas: any[]) => this.listaUsuarios = personas,
       error: (error: any) => console.log(error)
     })
+  }
+
+  isObject(v:any):boolean {
+    if(typeof v == 'object') {
+      return true
+    }
+    return false
+  }
+
+  convertir(v:any):string {
+    let resultado:string = ""
+    Object.keys(v).forEach(function(key) {
+      if (typeof v[key] == 'object') {
+        Object.keys(v[key]).forEach(function(k) {
+          resultado += k+": "+v[key][k]+ '\n'
+        })
+      } else {
+        resultado += key+": "+v[key]+ '\n'
+      }
+    })
+    return resultado
   }
 }
